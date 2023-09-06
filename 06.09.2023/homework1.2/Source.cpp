6e27842a519f9d0a6ebf5f8b89ebe5683dd56c17
st117164 <st117164@student.spbu.ru>
2023-09-06 22:20:25 +0300

    06.09.2023_2

---
+++ 06.09.2023/homework1.2/Source.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int N;
+	cin >> N;
+	cout << (1 + N) / 2 * N;
+	return 0;
+}
---
+++ 06.09.2023/homework1.3/Source.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int n;
+	cin >> n;
+	int n1 = n / 10;
+	int n2 = n1 + 1;
+	cout << n1 * n2 << 25;
+	return 0;
+}
